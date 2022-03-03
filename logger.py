f23f3261c39ca7cd3ad0b34a774b75ae8c41e72f
Eyal Izhaki <eyal1izhaki@gmail.com>
2022-03-03 13:59:24 +0200

    Logs to bot.log

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import logging
 # Setting logger up
 logger = logging.getLogger("botmanager")
 logger.setLevel(logging.INFO)
-logging_handler = logging.StreamHandler()
+logging_handler = logging.FileHandler('bot.log')
 logging_handler.setLevel(logging.INFO)    
 formatter = logging.Formatter("[%(name)s] [%(levelname)s] [%(asctime)s] - %(message)s")
 logging_handler.setFormatter(formatter)
